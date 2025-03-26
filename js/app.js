4685f3e74ff8f9c535a62c6d5c5ff106cbe885b6
alenorgue <alenoriegag@gmail.com>
2025-03-26 20:38:50 +0100

    finished with a function for each button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 // Tu código aquí.
+
+let count = 0;
+
+const btnLowerCount = document.querySelector('.prevBtn');
+const btnAddCount = document.querySelector('.nextBtn');
+
+btnLowerCount.addEventListener('click', function() {
+count-console.log('menos 1');
+count--;
+counter.textContent = count;
+
+if (count < 0 ){
+    counter.style.color = 'red'
+  } else if (count > 0){
+    counter.style.color = 'green'
+  } else {
+    counter.style.color = '#333333'
+  }
+});
+
+
+btnAddCount.addEventListener('click', function() {
+    count-console.log('mas 1');
+count++;
+counter.textContent = count;
+
+if (count < 0 ){
+    counter.style.color = 'red'
+  } else if (count > 0){
+    counter.style.color = 'green'
+  } else {
+    counter.style.color = '#333333'
+  }
+});
+
+const counter = document.querySelector('#counter')
+counter.textContent = count
+
+
